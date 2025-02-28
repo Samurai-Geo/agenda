2faa4f4c109536846f8bfcda16b4850f869614f6
Samurai-Geo <geoloarox@gmail.com>
2025-02-28 07:08:58 -0300

    primeiro update 28/02/2025

--- contact/admin.py
+++ contact/admin.py
@@ -5,14 +5,14 @@ from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'first_name', 'last_name', 'phone', 'category',
+        'id', 'first_name', 'last_name', 'phone', 'category', 'show',
     )
     ordering = '-id',
     list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name', 'phone', 'email',
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'phone',
+    list_editable ='show',
     list_display_links = 'first_name',
     # prepopulated_fields = 
 
@@ -20,3 +20,4 @@ class ContactAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     list_display = 'name',
     ordering = '-id',
+
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.all()
+    # contacts = Contact.objects.all().order_by('-id')
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('-id')[0:10]
 
     context = {
         'contacts': contacts,
@@ -11,5 +15,23 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context,
+    )
+
+def contact(request, contact_id):
+    # contacts = Contact.objects.all().order_by('-id')
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(Contact.objects, pk=contact_id, show=True)
+
+    if single_contact is None:
+        raise Http404()
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context,
     )
