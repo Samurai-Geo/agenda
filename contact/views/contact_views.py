663b56b0a13a966c3394710dcd172771eef21c94
Samurai-Geo <geoloarox@gmail.com>
2025-03-04 19:14:19 -0300

    Implementacao de header

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 def index(request):
@@ -10,6 +11,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - ',
     }
 
     return render(
@@ -18,16 +20,48 @@ def index(request):
         context,
     )
 
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(Q(first_name__icontains=search_value)| 
+                Q(last_name__icontains=search_value)|
+                Q(phone__icontains=search_value)|
+                Q(email__icontains=search_value)|
+                Q(id__icontains=search_value)
+                )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - ',
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+
+
 def contact(request, contact_id):
     # contacts = Contact.objects.all().order_by('-id')
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact.objects, pk=contact_id, show=True)
+
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     if single_contact is None:
         raise Http404()
 
     context = {
         'contact': single_contact,
+        'site_title': site_title,
     }
 
     return render(
