6ae2a44b00565132966710aa11e47ad72a3d9664
Samurai-Geo <geoloarox@gmail.com>
2025-03-08 17:03:38 -0300

    pag create com metade da aparencia

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -6,6 +6,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 
 def create(request):
+    if request.method == "POST":
+        print()
+        print(request.method)
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+        print()
+    
     context = {
 
     }
