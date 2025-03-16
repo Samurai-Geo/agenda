b0bb8de8752b0e2bd71417e7027ff7452544118c
Samurai-Geo <geoloarox@gmail.com>
2025-03-16 16:53:21 -0300

    criado pag update(falta finalizar)

--- contact/urls.py
+++ contact/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # contact (CRUD) Created, Read, Update, Delete
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
 
     # contact (CRUD) Created, Read, Update, Delete
     # path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
