095b856e7fe7974663631224d0ff0929b8a5a67d
Samurai-Geo <geoloarox@gmail.com>
2025-04-04 07:08:23 -0300

    pagina user corrigida

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages, auth
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
-from contact.forms import RegisterForm
+from django.shortcuts import redirect, render
 
+from contact.forms import RegisterForm, RegisterUpdateForm
 
 def register(request):
     form = RegisterForm()
@@ -28,6 +29,33 @@ def register(request):
         }
     )
 
+@login_required(login_url='contact:login')
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form.save()
+    return redirect('contact:user_update')
+ 
 def login_view(request):
 
     form = AuthenticationForm(request)
